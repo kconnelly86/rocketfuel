0676a366ed50921f06e4e783bc88c6906d45e3d7
kconnelly86 <kconnelly86@yahoo.com>
2018-03-12 19:17:26 -0500

    removed all old js modals and replaced the with Jquery modals and removed the divs aroudn the modals and have to rebuild and reposition them as well as style them.

--- rocket.js
+++ rocket.js
@@ -1,40 +1,12 @@
+// jQuery instead of JS
+$(".button").on("click", function () {
+    var modal = $(this).data("modal");
+    $(modal).show();
+});
 
-// Get the modal
-var modal1 = document.getElementById('myModal1');
-var modal2 = document.getElementById('myModal2');
-var modal3 = document.getElementById('myModal3');
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("myBtn1");
-var btn2 = document.getElementById("myBtn2");
-var btn3 = document.getElementById("myBtn3");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn1.onclick = function () {
-    modal1.style.display = "block";
-    console.log(onclick);
-}
-btn2.onclick = function () {
-    modal2.style.display = "block";
-     console.log(onclick);
-}
-btn3.onclick = function () {
-    modal3.style.display = "block";
-     console.log(onclick);
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal1.style.display = "none";
-    console.log(span);
-}
-
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+$(".modal").on("click", function (e) {
+    var className = e.target.className;
+    if (className === "modal" || className === "close") {
+        $(this).closest(".modal").hide();
+    }
+});
